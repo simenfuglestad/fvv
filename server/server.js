20af27323d7d80d537aab65a7a92831401e8963f
Bjørn Martin Sørensen <bjosor@norkart.no>
2020-07-09 09:28:48 +0200

    Production build, first try

--- server/server.js
+++ server/server.js
@@ -104,5 +104,14 @@ app.get('/getCaseList', (req,res) => {
   res.send(cases)
 })
 
+if (process.env.NODE_ENV === 'production') {
+  // Serve any static files
+  app.use(express.static(path.join(__dirname, 'client/build')));
+  // Handle React routing, return all requests to React app
+  app.get('*', function(req, res) {
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+  });
+}
+
 app.listen(port, () => console.log(`Listening on port ${port}`));
 
